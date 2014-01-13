ba96eedca47c7c5fe950b5affc6fb68f391e14ff
Filip <filipmilkovic47@gmail.com>
2014-01-13 22:03:09 +0100

    Switched comments Added user input and program does something useful, kind of.

--- src/bestClass.java
+++ src/bestClass.java
@@ -1,23 +1,36 @@
 /**
  *Created by Filip Milković
- *Program does nothing useful, it is just for testing and learning Java so I could work on Android and Minecraft in the future
- *Dates working on program: 03.01.2014 ;
+ *Program does nothing useful yet, it is just for testing and learning Java so I could work on Android and other stuff in the future
+ *And I am waiting for JetBrains C/C++ editor and I hope it will be free
+ *Dates working on program: 03.01.2014 ; 13.01.2014 ;
  *Licence: *NONE*
  */
+import java.util.Scanner;
 
-public  class bestClass {
+public  class bestClass  {
     public static void main(String[] args){
+
 //    testing types of numbers
-        int number= 0b110011;
-        number-=100;
-        System.out.println("Number from binary system to decimal is: " + number);
-//    text output
-        System.out.println("Yayyy");
-        System.out.println("Yayyy");
-        System.out.println("I need to learn to code something new xD, this is getting boring");
-        System.out.println("Arghhhhh I need to learn and concentrate ");
+        int i;
+        int n;
+        int number;
+
+//get user input for number
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        number=reader.nextInt();
+
+        System.out.println("Number is: " + number);
+
+//get user input for n
+        Scanner read = new Scanner(System.in);
+        System.out.println("Enter the number of repeats: ");
+        n=read.nextInt();
+
+
+
 //    if | else if | else statement
-        if(number<0){                                                                                   //No need for brackets, but it is easier to read and edit in the future coding
+        if(number<0){
             System.out.println("Hello world!");
             number+= 0x69;
             System.out.println(number);
@@ -28,10 +41,15 @@ public  class bestClass {
         else{
             System.out.println("Hello Samobor!");
         }
+
+//    For statement
+        for(i=1;i<=n;i++){
+            System.out.println("I can print this how many times times you want, and I know, it is lame...");
+        }
     }
 }
 
-
+//examples from "http://docs.oracle.com/javase/tutorial/index.html"
 
 /*public class bestClass {
     public static void main(String[] args) {
